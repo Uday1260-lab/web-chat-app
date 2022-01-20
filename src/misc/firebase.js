b8e49108a609fbba946a9a417e74c249a1d00763
Uday1260-lab <udayp2k18@gmail.com>
2022-01-20 22:09:27 +0530

    Initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const config = {
+  apiKey: "AIzaSyD6Ddikzs_2ffeA7mMPV18KQHYhA5Pg3m8",
+  authDomain: "web-chat-app-f20c3.firebaseapp.com",
+  databaseURL: "https://web-chat-app-f20c3-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "web-chat-app-f20c3",
+  storageBucket: "web-chat-app-f20c3.appspot.com",
+  messagingSenderId: "762822930785",
+  appId: "1:762822930785:web:fc065bf3b17b3a0588d520"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(config);
