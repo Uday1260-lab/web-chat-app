2e8778ada73870617ec4ed1af20677a83e8ea018
Uday1260-lab <udayp2k18@gmail.com>
2022-01-24 20:25:56 +0530

    Displayed all messages in chat room  page

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,9 +1,36 @@
-import React from 'react';
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { database } from '../../../misc/firebase';
+import { transformToArrWithId } from '../../../misc/helpers';
+import MessageItem from './MessageItem';
 
-const Messages = () => {
-  return <div>
-      Messages
-    </div>;
+const messages = () => {
+    
+    const { chatId } = useParams()
+    const [messages,setMessages] = useState(null);
+
+    const isChatEmpty = messages && messages.length === 0 ;
+    const canShowMessages = messages && messages.length > 0 ;
+
+    useEffect(() => {
+        const messagesRef = database.ref('/messages');
+        messagesRef.orderByChild('roomId').equalTo(chatId).on('value', (snap) => {
+        const data = transformToArrWithId(snap.val());
+        setMessages(data);
+        });
+
+
+
+      return () => {
+          messagesRef.off('value');
+      }
+    }, [chatId]);
+    
+    return <ul className='msg-list custom-scroll'>
+      { isChatEmpty && <li> No Messages yet :/ </li>  }
+      { canShowMessages && messages.map( msg => < MessageItem key={msg.id} message={msg} /> ) }
+    </ul>;
 };
 
-export default Messages;
+export default messages;
