13f008c906c370eeded842ad34c93758c8e108d9
Uday1260-lab <udayp2k18@gmail.com>
2022-01-26 20:52:48 +0530

    Displayed audio messages and fix file deletion when message is being deleted

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -19,6 +19,13 @@ const renderFileMessage = (file) => {
             </div>
         )
     }
+
+    if (file.contentType.includes('audio')) {
+        return <audio controls>
+            <source src={file.url} type="audio/mp3" />
+            Your Browser does not support audio
+        </audio>
+    }
     
     return (
         <a href={file.url} > DownLoad {file.name} </a>
@@ -83,7 +90,7 @@ const MessageItem = ({ message, handleAdmin, handleLike , handleDelete }) => {
                         isVisible={canShowIcons}
                         iconName='close'
                         tooltip="Delete this message"
-                        onClick={() => handleDelete(message.id) }
+                        onClick={() => handleDelete(message.id,file) }
                     />
                 )
             }
--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -2,7 +2,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Alert } from 'rsuite';
-import { auth, database } from '../../../misc/firebase';
+import { auth, database, storage } from '../../../misc/firebase';
 import { transformToArrWithId } from '../../../misc/helpers';
 import MessageItem from './MessageItem';
 
@@ -86,7 +86,7 @@ const Messages = () => {
 
 
   const handleDelete = useCallback(
-    async msgId => {
+    async (msgId,file) => {
       if (!window.confirm('Delete this message?')) {
         return;
       }
@@ -118,8 +118,19 @@ const Messages = () => {
         await database.ref().update(updates);
         Alert.info('Message has been deleted');
       } catch (err) {
-        Alert.error(err.message);
+        return Alert.error(err.message);
       }
+
+      if(file){
+        try {
+            const fileRef = storage.refFromURL(file.url)
+            await fileRef.delete();
+        } catch (err) {
+          Alert.error(err.message,5000);        
+        }
+      }
+
+     
     },
     [chatId, messages]
   );
