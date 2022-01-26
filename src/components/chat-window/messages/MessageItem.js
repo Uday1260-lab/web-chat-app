009f00273499447a8715f17557f8719e6c2212f8
Uday1260-lab <udayp2k18@gmail.com>
2022-01-26 14:16:49 +0530

    Helped message likes

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -2,18 +2,18 @@ import React, { memo } from 'react';
 import { Button } from 'rsuite';
 import TimeAgo from 'timeago-react';
 import { useCurrentroom } from '../../../context/current-room.context';
-import { useHover } from '../../../misc/custom-hooks';
+import { useHover, useMediaQuery } from '../../../misc/custom-hooks';
 import { auth } from '../../../misc/firebase';
 import PresenceDot from '../../PresenceDot';
 import ProfileAvatar from '../../ProfileAvatar';
 import IconBtnControl from './IconBtnControl ';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
-const MessageItem = ({message,handleAdmin}) => {
-
-    const {author , createdAt , text } = message;
-
+const MessageItem = ({ message, handleAdmin, handleLike }) => {
+    const { author, createdAt, text, likes, likeCount } = message;
     const [selfRef,isHover] = useHover();
+
+    const isMobile = useMediaQuery('(max-width: 992px)');
 
     const isAdmin = useCurrentroom( v => v.isAdmin );
 
@@ -24,6 +24,10 @@ const MessageItem = ({message,handleAdmin}) => {
     const isAuthor = auth.currentUser.uid === author.uid;
 
     const canGrantAdmin = isAdmin && !isAuthor;
+
+    const canShowIcons = isMobile || isHover ;
+
+    const isLiked = likes && Object.keys(likes).includes(auth.currentUser.uid)
 
     return <li className={` padded mb-1 cursor-pointer ${isHover ? 'bg-black-02' : ''} `} ref={selfRef} >
         <div className='d-flex align-items-center font-bolder mb-1'>
@@ -50,12 +54,12 @@ const MessageItem = ({message,handleAdmin}) => {
                 className='font-normal text-black-45 ml-2' 
             />
             <IconBtnControl
-                {...(true ? {color : 'blue'} : {})} 
-                isVisible
+                {...( isLiked ? {color : 'green'} : {})} 
+                isVisible={canShowIcons}
                 iconName='heart'
                 tooltip="Like this message"
-                onClick={() => {}}
-                badgeContent={5}
+                onClick={() => handleLike(message.id) }
+                badgeContent={likeCount}
             />
         </div>
         <div>
--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -2,7 +2,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Alert } from 'rsuite';
-import { database } from '../../../misc/firebase';
+import { auth, database } from '../../../misc/firebase';
 import { transformToArrWithId } from '../../../misc/helpers';
 import MessageItem from './MessageItem';
 
@@ -51,10 +51,39 @@ const messages = () => {
 
     }, [chatId]);
     
+    const handleLike = useCallback( async (msgId) => {
+      const  {uid} = auth.currentUser
+      const messageRef = database.ref(`/messages/${msgId}`);
+
+      let alertMsg;
+
+      await messageRef.transaction( msg => {
+        if (msg) {
+          if (msg.likes && msg.likes[uid] ) {
+            msg.likeCount = msg.likeCount ? msg.likeCount - 1 : 0 ;
+            msg.likes[uid] = null ;
+            alertMsg = "Like Removed"
+          } else {
+            msg.likeCount = msg.likeCount ? msg.likeCount + 1 : 1;
+            if(!msg.likes){
+              msg.likes = {};
+            }
+
+            msg.likes[uid] = true ;
+            alertMsg = "Liked"
+          }
+        }
+        return msg ;
+      });
+
+      Alert.info(alertMsg,5000)
+      
+    }, []);
+    
 
     return <ul className='msg-list custom-scroll'>
       { isChatEmpty && <li> No Messages yet :/ </li>  }
-      { canShowMessages && messages.map( msg => < MessageItem key={msg.id} message={msg} handleAdmin={handleAdmin} /> ) }
+      { canShowMessages && messages.map( msg => < MessageItem key={msg.id} message={msg} handleAdmin={handleAdmin}  handleLike={handleLike}/> ) }
     </ul>;
 };
 
